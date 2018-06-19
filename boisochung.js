800d9afea3cedf6fbb87b05793d6385866973d1a
mai <huynhtrucmai218@gmail.com>
2018-06-18 22:30:47 -0400

    bsc trong n so

--- boisochung.js
+++ boisochung.js
@@ -1,17 +1,12 @@
-//tim boi so chung cua 2 so trong day boi so cho truoc(0<= n<=10)
-var boiso1=require('./boiso');
-function boisochungar(a,b){
-	//var a1=boiso1(a);
-	var a1=boiso1(b);
-	var boisochunga=[];
-	for( var n=1; n<=10; n++)
+//tim boi so chung cua 2 so trong n so(0<= n<=100)
+function boisochung(a,b){
+	for( var n=1; n<=100; n++)
 		{
-			if(a1[n]%a==0 && a1[n]%b==0)
+			if(n%a==0 && n%b==0)
 			{
-				boisochunga.push(a1[n]);
+					console.log(n);
 			}
 		}		
-		console.log(boisochunga);
-       return boisochunga;
+       return n;
  }
-boisochungar(2,4);
+boisochung(2,5);
