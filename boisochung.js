2c909a4ffc7ad620b7609657efa3758ffb4eb858
mai <huynhtrucmai218@gmail.com>
2018-06-19 18:39:11 +0800

    bscnn

--- boisochung.js
+++ boisochung.js
@@ -5,8 +5,22 @@ function boisochung(a,b){
 			if(n%a==0 && n%b==0)
 			{
 					console.log(n);
+				
 			}
 		}		
        return n;
  }
 boisochung(2,5);
+
+ function bscnn(a,b){
+	 for(var n=1; n<= 100; n++)
+	 {
+		 if(n%a==0&& n%b==0)
+		 { 
+			console.log("bscnn cua ",a,b, "la ",n);
+			return;
+		 }
+	 }
+ }
+ bscnn(2,3);
+ 
